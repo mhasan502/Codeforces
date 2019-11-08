31424a017baf63397f389dc9bd4ad0bf46126841
Mehedi Hasan <mhasan502@gmail.com>
2019-11-08 20:15:00 +0600

    Update 1A - Theatre Square.cpp

--- 1A - Theatre Square.cpp
+++ 1A - Theatre Square.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main(){
 
-    long long n,m,a,na,ma,sum;
+    long long n,m,a;
     cin >> n >> m >> a;
-    
+    long long na,ma,sum;
     if(n%a)
         na = n/a+1;
     else
